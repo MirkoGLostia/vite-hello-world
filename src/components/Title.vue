6d338fa196754d600b6d02fc274e28c15fcce92f
MirkoGLostia <giggi.torre@mail.com>
2023-05-30 16:29:09 +0200

    title component

---
+++ src/components/Title.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default {
+
+    name: "Title"
+
+}
+
+</script>
+
+<template>
+    <h1>
+        La mia prima app con Vite!
+    </h1>
+</template>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
